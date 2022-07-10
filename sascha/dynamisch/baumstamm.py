6718e41f903dbf901b1c4a577ebef0372497f9be
v1lling <sascha.villing@web.de>
2022-07-10 20:42:53 +0200

    knapper knapsack du sack

--- sascha/dynamisch/baumstamm.py
+++ sascha/dynamisch/baumstamm.py
@@ -6,19 +6,36 @@ from icecream import ic
 # 
 # G[k] = max(G[k-1] | n/k * x[k])
 
-# IST DAS RICHTIG?
-# IDK!
-n = 4
-x = [0,2,3,8,9]
+# am 0-1-Rucksack Problem orientiert!
+
+n = 5
+x = [0,2,3,8,9,0]
 def zuschnitt():
-    G = []
-    G = np.zeros(n+1)
-    G[0] = 0 # Basisfall
-    for i in range(1, n+1):
-        times = int(n/i)
-        rest = n % i
-        G[i] = max(G[i-1], times*x[i] + x[rest] ) # Rekursionsformel
+    G = np.zeros((n+1, n+1))
+    for i in range(0, n+1):
+        G[i][0] = 0 # Basisfall
+        ic(i)
+        for k in range(0, n+1):
+            if (i==0):
+                G[i][k] = 0
+            else:
+                rest = i - k
+                ic(rest)
+                if rest < 0:
+                    G[i][k] = G[i][k-1]
+                else:
+                    G[i][k] = max(G[i][k-1], x[k] + G[rest][k])
     return G
 ic(
     zuschnitt()
 )    
+
+#       k 0 -> n
+#  i 0  [0 0 0 0 0 0 0]
+#  0 1  [0 2 2 2 2 2 2
+#  | 2  [0 4 4 4 4 4 4
+#  v 3  [0 6                max(6,3+2)            
+#  n 4
+#    5
+#
+#
